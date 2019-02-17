b6036995e30460b1ec513c25a4efdc3c7e98dbac
EmmanueTobi <emmanuel.a.oluwatobi@gmail.com>
2019-02-17 10:40:01 +0100

    Cost calculation improvement for items

--- app/src/main/java/com/mofe/activities/home_activity.kt
+++ app/src/main/java/com/mofe/activities/home_activity.kt
@@ -1,35 +1,29 @@
 package com.mofe.activities
 
 import android.annotation.SuppressLint
-import android.annotation.TargetApi
 import android.app.Activity
-import android.app.Dialog
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.EditText
+import android.widget.RelativeLayout
 import com.mofe.R
 import com.mofe.adapters.ItemsAdapter
 import com.mofe.database.AppDatabase
 import com.mofe.database.entities.Items
-import com.mofe.utils.NumberAmountFormat
-import com.mofe.utils.RecyclerItemClickListener
+import com.mofe.utils.*
 import com.mofe.utils.SharedprefManager.amount
 import com.mofe.utils.SharedprefManager.customPreference
 import com.mofe.utils.SharedprefManager.init_amount
 import com.mofe.utils.SharedprefManager.lastdate
-import com.mofe.utils.getCurrentDateTime
-import com.mofe.utils.toString
+import com.mofe.utils.SharedprefManager.spentamount
 import kotlinx.android.synthetic.main.activity_home.*
-import kotlinx.android.synthetic.main.item_click_option_view.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
@@ -38,15 +32,18 @@ import org.jetbrains.anko.uiThread
  * Created by ${cosmic} on 2/10/19.
  */
 
-class home_activity : AppCompatActivity() {
+open class home_activity : AppCompatActivity() {
 
-    val CUSTOM_PREF_NAME = "amount_data"
-    val mActivity: Activity = this@home_activity
+    open val CUSTOM_PREF_NAME = "amount_data"
+    open val mActivity: Activity = this@home_activity
+
     private var mAdapter: ItemsAdapter? = null
     val context: Context = this
 
     lateinit var rvMofe: RecyclerView
+    lateinit var rView: RelativeLayout
     var mArrayList: ArrayList<Items> = ArrayList()
+    lateinit var database : AppDatabase
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -55,23 +52,27 @@ class home_activity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
 
         val Prefs = customPreference(this, CUSTOM_PREF_NAME);
+        database = AppDatabase.getInstance(context = this@home_activity)
 
         doAsync {
 
-            val database = AppDatabase.getInstance(context = this@home_activity)
-            val bills = database.ItemsDao().all
-            mArrayList = bills as ArrayList<Items>
+            val items = database.ItemsDao().loadAllByGotten("no")
+            sortingforItmes(items)
+            mArrayList = items as ArrayList<Items>
+            rvMofe = mofe_rv
+            rView = no_items_added
 
             uiThread {
+
                 if(mArrayList.size == 0) {
 
-                    mofe_rv.visibility = View.GONE
-                    no_items_added.visibility = View.VISIBLE
+                    rvMofe.visibility = View.GONE
+                    rView.visibility = View.VISIBLE
 
                 } else {
-                    mofe_rv.visibility = View.VISIBLE
-                    no_items_added.visibility = View.GONE
-                    mAdapter = ItemsAdapter(this@home_activity, mArrayList)
+
+                    rView.visibility = View.GONE
+                    mAdapter = ItemsAdapter(this@home_activity, mArrayList,this@home_activity, false)
 
                     initializeMoferv()
 
@@ -88,7 +89,7 @@ class home_activity : AppCompatActivity() {
         init_amt_input.setText(NumberAmountFormat(Prefs.init_amount))
 
         edit_amt.setOnClickListener {
-            dialogUpdate(this);
+            dialogUpdate(this, false, null);
         }
 
         add_item_toget_fab.setOnClickListener {
@@ -98,75 +99,45 @@ class home_activity : AppCompatActivity() {
 
         cp_bar.run {
             setRounded(true)
-            setMaxProgress(80F)
+            setMaxProgress(Prefs.init_amount.toFloat())
             setProgressColor(context.getColor(R.color.lime_progress_100))
             setProgressBackgroundColor(context.getColor(R.color.colorWhite))
             setProgressWidth(16.0F)
-            setProgress(20.0F)
+            setProgress(Prefs.spentamount)
         }
     }
 
     private fun initializeMoferv() {
 
         rvMofe = mofe_rv
+        rvMofe.visibility = View.VISIBLE
         rvMofe.setHasFixedSize(true)
         rvMofe.layoutManager = LinearLayoutManager(this)
         rvMofe.adapter = mAdapter
 
         rvMofe.addOnItemTouchListener(RecyclerItemClickListener(baseContext, rvMofe, object : RecyclerItemClickListener.OnItemClickListener {
 
-                    override fun onItemClick(view: View, position: Int) {
-                        OpenItemDialog(context, mArrayList[position], position)
-                    }
+                    override fun onItemClick(view: View, position: Int) { }
 
                     override fun onLongItemClick(view: View, position: Int) {
 
+                        val holder: ItemsAdapter.ViewHolder = ItemsAdapter.ViewHolder(view)
+
+                        if (holder.optionsItemClick.visibility == View.GONE){
+                            holder.optionsItemClick.visibility = View.VISIBLE
+                        } else {
+                            holder.optionsItemClick.visibility = View.GONE
+                        }
                     }
                 })
         )
-    }
-
-    @TargetApi(Build.VERSION_CODES.M)
-    fun OpenItemDialog(context: Context, items: Items, position: Int){
-
-        val wmLayoutParams = window.attributes
-        wmLayoutParams.gravity = Gravity.CENTER_HORIZONTAL
-        window.attributes = wmLayoutParams
-        val dialog = Dialog(context)
-        dialog.setContentView(R.layout.item_click_option_view)
-
-        val checked_gotten = dialog.checked_gotten
-        val edit_price = dialog.edit_price
-        val delete_item = dialog.delete_item
-        val mofe_view_item = dialog.picked_mofe
-
-        mofe_view_item.setTextColor(context.getColor(R.color.colorAccent))
-        mofe_view_item.setText(items.itemName)
-
-        checked_gotten.setOnClickListener {
-
-        }
-
-        edit_price.setOnClickListener {
-            AppDatabase.getInstance(this@home_activity).ItemsDao().update(300, position)
-            toast("Clicking")
-
-        }
-
-        delete_item.setOnClickListener {
-
-        }
-
-        dialog.setCanceledOnTouchOutside(true)
-        dialog.show()
-
     }
 
     /**
      * dialog to add category
      * */
     @SuppressLint("ResourceAsColor")
-    fun dialogUpdate(context: Context) {
+    fun dialogUpdate(context: Context, forItem: Boolean, position: Int?) {
 
         val lytInf = LayoutInflater.from(context)
         val promptsView = lytInf.inflate(R.layout.alert_dialog_update_amount, null)
@@ -176,43 +147,62 @@ class home_activity : AppCompatActivity() {
         alert.setView(promptsView)
 
         val input: EditText = promptsView.findViewById(R.id.edtAmt) as EditText
-
         alert.setPositiveButton("Update", { _, _ -> })
-
         alert.setNegativeButton("Cancle", { _, _ -> })
-        val alertDialog = alert.create()
 
+        val alertDialog = alert.create()
         alertDialog.setOnShowListener {
 
             val button_positive = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)
             val button_negative = alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE)
-            button_positive.setTextColor(context.resources.getColor(R.color.colorPrimary))
 
+            button_positive.setTextColor(context.resources.getColor(R.color.colorPrimary))
             button_positive.setOnClickListener {
 
                 val amt_update: String = input.text.toString().trim()
 
-                if(amt_update != ""){
+                if(!forItem){
 
-                    if(init_amt_input.text.toString() != "update amount"){
+                    if(amt_update != ""){
 
-                        val new_update = Prefs.amount + amt_update.toInt()
-                        init_amt_input.setText(NumberAmountFormat(new_update))
-                        Prefs.amount = new_update
-                        Prefs.init_amount = new_update
+                        if(init_amt_input.text.toString() != "update amount"){
+
+                            val new_update = Prefs.init_amount + amt_update.toInt()
+                            init_amt_input.setText(NumberAmountFormat(new_update))
+                            Prefs.init_amount = new_update
+                        }
+
+                        else {
+
+                            init_amt_input.setText(NumberAmountFormat(amt_update.toInt()))
+                            Prefs.init_amount = amt_update.toInt()
+                        }
+
+                        alertDialog.dismiss()
                     }
 
-                    else {
+                    else { toast("Enter Amount to update or add") }
 
-                        init_amt_input.setText(NumberAmountFormat(amt_update.toInt()))
-                        Prefs.init_amount = amt_update.toInt()
+                } else {
+
+                    if(amt_update != ""){
+
+                        if (position != null) {
+
+                            val items: Items = database.ItemsDao().findItemById(position)!!
+                            Prefs.amount = Prefs.amount + items.itemPrice!!
+
+                            items.itemPrice = amt_update.toInt()
+                            Prefs.amount = Prefs.amount - items.itemPrice!!
+                            amt_reduction.setText(Prefs.amount.toString())
+                            database.ItemsDao().update(items)
+                        }
+                    }
+                    else {
+                        toast("Enter amount to change")
                     }
 
                     alertDialog.dismiss()
-                }
-                else {
-
-                    toast("Enter Amount to update or add")
                 }
             }
 
